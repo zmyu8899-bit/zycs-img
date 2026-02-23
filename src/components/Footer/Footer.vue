2b3a34b253c8e59c04a23b0792338977ffc5a91a
zmyu8899-bit <zmyu8899@gmail.com>
2026-02-24 03:30:29 +0800

    Update Footer.vue

--- src/components/Footer/Footer.vue
+++ src/components/Footer/Footer.vue
@@ -2,7 +2,7 @@
   <footer class="w-full max-w-[1666px] mt-36 pb-4">
     <main class="m-auto pt-4 flex flex-col-reverse md:flex-row justify-between gap-2 md:gap-16 w-full max-w-[1666px] border-t">
       <section class="[&>p]:text-xs [&>p]:w-full [&>p]:py-[6px] [&>p]:text-center md:[&>p]:text-left [&>p>a]:text-slate-400">
-        <p>骤雨重山图床 是 <a href="https://www.vvhan.com" target="_blank" title="韩小韩博客">韩小韩博客</a> 支持并维护的文件上传项目，致力于为用户提供稳定的永久存储服务。</p>
+        <p>星云图床 是 <a href="https://www.nebulat.top/" target="_blank" title="星云博客">星云博客</a> 支持并维护的文件上传项目，致力于为用户提供稳定的永久存储服务。</p>
         <p class="text-red-600">注意:上传违反中国大陆、香港及美国法律的图片将会直接删除，并封禁设备IP。</p>
         <p class="flex gap-2 justify-center md:justify-start">
           <a href="https://pages.cloudflare.com" target="_blank">
@@ -24,17 +24,17 @@
       </section>
       <section class="flex flex-col items-center gap-2">
         <div class="flex gap-6">
-          <a title="打赏站长" target="_blank" href="/reward.webp">
+          <a title="打赏站长" target="_blank" href="/reward.web">
             <img class="w-8" src="@/assets/images/footer/shang.svg" />
           </a>
           <a title="Github" target="_blank" rel="nofollow" href="https://github.com/uxiaohan">
             <img class="w-8" src="@/assets/images/footer/github.svg" />
           </a>
-          <a title="韩小韩博客" target="_blank" href="https://www.vvhan.com/">
+          <a title="星云博客" target="_blank" href="https://www.nebulat.top/">
             <img class="w-8" src="@/assets/images/footer/blog.svg" />
           </a>
         </div>
-        <span class="w-full font-bold text-lg text-center">骤雨重山图床</span>
+        <span class="w-full font-bold text-lg text-center">星云图床</span>
       </section>
     </main>
   </footer>
